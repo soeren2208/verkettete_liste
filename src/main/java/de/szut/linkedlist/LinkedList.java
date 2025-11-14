287a65c9eac00722223f1951ec93e6c9fab244c2
Sören Schwerk <s.schwerk@schule.bremen.de>
2025-11-14 11:48:53 +0100

    nach dem zweiten Termin

--- src/main/java/de/szut/linkedlist/LinkedList.java
+++ src/main/java/de/szut/linkedlist/LinkedList.java
@@ -12,9 +12,52 @@ public class LinkedList<T> {
         return first == null;
     }
 
-
-    private class Node {
-
+    public void add(T value) {
+        if(isEmpty()){
+            this.first = new Node(value);
+        }
+        else{
+            Node current = findNodeAtIndex(size-1);
+            current.next = new Node(value);
+        }
+        this.size++;
     }
 
+    private Node findNodeAtIndex(int index){
+        Node current = this.first;
+        for(int i = 0; i < index; i++){
+            current = current.next;
+        }
+        return current;
+    }
+
+    public T get(int index) {
+        if(isEmpty()){
+            return null;
+        }
+        else{
+            return findNodeAtIndex(index).value;
+        }
+    }
+
+    public T remove(int index) {
+        if(isEmpty()){
+            return null;
+        }
+        else{
+            Node current = first;
+            first = null;
+            this.size--;
+            return current.value;
+        }
+    }
+
+    private class Node {
+        private T value;
+        private Node next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
 }
--- src/test/java/de/szut/linkedlist/LinkedListTest.java
+++ src/test/java/de/szut/linkedlist/LinkedListTest.java
@@ -1,24 +1,67 @@
 package de.szut.linkedlist;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class LinkedListTest {
+    private LinkedList<Integer> list;
+
+    @BeforeEach
+    void setUp(){
+        this.list = new LinkedList();
+    }
 
     @Test
     void givenEmptyList_whenSize_thenZero(){
-        LinkedList list = new LinkedList();
         assertThat(list.size()).isEqualTo(0);
     }
 
     @Test
     void givenEmptyList_WhenIsEmpty_thenTrue(){
-        LinkedList list = new LinkedList();
         assertThat(list.isEmpty()).isTrue();
     }
 
+    @Test
+    void givenEmptyList_WhenAdd_ThenReturnValidValueAndSizeOneAndIsNotEmpty(){
+        list.add(6);
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.isEmpty()).isFalse();
+        assertThat(list.get(0)).isEqualTo(6);
+    }
 
+    @Test
+    void givenListSizeOne_WhenAdd_ThenReturnValidValuesAndSizeTwo(){
+        list.add(6);
+        list.add(7);
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.get(0)).isEqualTo(6);
+        assertThat(list.get(1)).isEqualTo(7);
+    }
 
+    @Test
+    void givenListEmptyList_WhenRemove_ThenReturnNull(){
+        assertThat(list.remove(0)).isEqualTo(null);
+    }
 
+    @Test
+    void givenListSizeOne_WhenRemove_ThenReturnValidValueAndSizeZero(){
+        list.add(6);
+        assertThat(list.remove(0)).isEqualTo(6);
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Test
+    @Disabled
+    void givenListContainingThreeValues_WhenRemoveAtIndexOne_ThenSizeIsTwoAndReturnValidValues(){
+        list.add(6);
+        list.add(7);
+        list.add(3);
+        list.remove(1);
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.get(0)).isEqualTo(6);
+        assertThat(list.get(1)).isEqualTo(3);
+    }
 }
